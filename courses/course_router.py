47bfb3099b98a7f48592d5a9774fe56f287ce19c
admin <denis.lamochenko@gmail.com>
2026-04-06 06:26:42 +0800

    refactor: add filters in course_router.py

--- courses/course_router.py
+++ courses/course_router.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Depends, HTTPException
+from typing import Optional, List
+from sqlalchemy import select, desc, asc, or_
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy import select, insert, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
@@ -7,6 +9,7 @@ from database.database import get_session
 from models.CourseModel import Course
 from shemas.course_shema import CreateCourse, ResponseCourse, UpdateCourse
 from shemas.lesson_shema import ResponseLesson
+from models.CourseEnum import CoursesCategory, CoursesLevel
 
 
 
@@ -24,10 +27,58 @@ async def create_courses(data: CreateCourse, session:AsyncSession = Depends(get_
     return new_course
 
 # Получение всех курсов
-@app.get("/get_courses")
+@app.get("/get_courses_all")
 async def get_courses(session:AsyncSession = Depends(get_session)):
     courses = await session.scalars(select(Course))
     return courses.all()
+
+@app.get("/get_courses", response_model=List[ResponseCourse])
+async def get_courses(# Фильтры
+        category: Optional[CoursesCategory] = None,
+        level: Optional[CoursesLevel] = None,
+        is_published: Optional[bool] = Query(True), # По умолчанию показываем опубликованные
+        search: Optional[str] = None,
+        
+        # Сортировка и пагинация
+        sort_by: str = Query("created_at", enum=["created_at", "title"]),
+        sort_order: str = Query("desc", enum=["asc", "desc"]),
+        limit: int = Query(10, gt=0, le=100),
+        offset: int = Query(0, ge=0),
+        
+        session: AsyncSession = Depends(get_session)
+    ):
+        # 1. Базовый запрос
+        query = select(Course)
+
+        # 2. Динамические фильтры
+        if category:
+            query = query.where(Course.category == category)
+        
+        if level:
+            query = query.where(Course.level == level)
+            
+        if is_published is not None:
+            query = query.where(Course.is_published == is_published)
+
+        if search:
+            # Поиск по подстроке в названии (регистронезависимый в PostgreSQL через ilike)
+            query = query.where(Course.title.ilike(f"%{search}%"))
+
+        # 3. Логика сортировки
+        sort_attr = getattr(Course, sort_by)
+        if sort_order == "desc":
+            query = query.order_by(desc(sort_attr))
+        else:
+            query = query.order_by(asc(sort_attr))
+
+        # 4. Пагинация (лимит и смещение)
+        query = query.limit(limit).offset(offset)
+
+        # 5. Выполнение
+        result = await session.execute(query)
+        courses = result.scalars().all()
+
+        return courses
 
 # Получение курса по id
 @app.get("/get_course/{course_id}", response_model=ResponseCourse)
